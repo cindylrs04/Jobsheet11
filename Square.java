839523d8e2f9b279adee9e1c02304a2a288ff721
cindylrs04 <larasaticindy231@gmail.com>
2023-11-06 13:42:38 +0700

    Square

--- Square.java
+++ Square.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
-public class Star {
+
+public class Square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
 
-        for(int i = 1; i <= N; i++) {
-            System.out.print("*");
-        }
         for (int iOuter = 1; iOuter <= N; iOuter++) {
             for (int i = 1; i <= N; i++) {
                 System.out.print("*");
@@ -16,6 +14,8 @@ public class Star {
             System.out.println();
             
         }
-    } 
+        
+
+    }
     
 }
